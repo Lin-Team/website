6169893c23fc46978ac4ae042cab0a21475e2392
Ivan <kezin636@gmail.com>
2023-09-26 01:19:26 +0300

    ready for deployment v5

--- src/components/index/TeamDescription.svelte
+++ src/components/index/TeamDescription.svelte
@@ -7,6 +7,11 @@
     <h3 class="card-title fw-bold">О нас</h3>
     <p class="card-text">
         { @html descriptionText }
+        <br /><br />
+        <a class="d-flex align-items-center" href="https://t.me/linteamnews" target="_blank">
+        	<i class="bi bi-telegram me-2 fs-2"></i>
+        	Telegram
+        </a>
     </p>
 </div>
 
